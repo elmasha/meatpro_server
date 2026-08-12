b9b56b61c8a1353c0d20f2a3456c7c6b970dc632
elmasha <elmashavitran@gmail.com>
2026-08-12 21:51:22 +0300

    ASD

--- routes/dailyOperationRoutes.js
+++ routes/dailyOperationRoutes.js
@@ -5,6 +5,6 @@ const controller = require('../controllers/dailyOperationController');
 router.post('/daily-operations', controller.createOrUpdateDailyOperation);
 router.get('/daily-operations/last', controller.getLastEntry);        // FIXED: was getEntryByDate
 router.get('/daily-operations/:date', controller.getEntryByDate);     // ADDED: was orphaned
-router.get('/daily/:date/totals', controller.getDateTotals);
+// router.get('/daily/:date/totals', controller.getDateTotals);
 
 module.exports = router;
